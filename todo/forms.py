559f5b312a68c666c763bbe08273264485ae70c9
Muhammad Abdullah Abid <hi@mabdullahabid.com>
2021-11-26 21:15:05 +0500

    add tasks

---
+++ todo/forms.py
@@ -0,0 +1,12 @@
+# in todo/forms.py
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ("name",)
+        labels = {
+            "name": "Add new task:",
+        }
