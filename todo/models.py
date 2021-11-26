a8652650dd4eb07115c7061eab79e55bbbbfdd06
Muhammad Abdullah Abid <hi@mabdullahabid.com>
2021-11-26 19:50:59 +0500

    first commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), related_name="tasks", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
